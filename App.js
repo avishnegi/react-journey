1efcef0a93182659fa4c1af04bd2e0aa5465af17
Avish Singh Negi <avish.singhnegi@gmail.com>
2024-05-08 08:30:56 +0530

    React Component Created

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
+/* const parent = React.createElement(
     "div",{id:"parent"},React.createElement(
         "div",{id:"child"},React.createElement(
             "h1",{},"Hi from H1")))
 // const heading = React.createElement("h1",{},"hello from React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(parent); */
+
+const HeadingComponent = () => {
+    return <h1>Hi from functional component</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HeadingComponent/>);
